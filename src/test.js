a76093fafcbe3158279649eaf5e2de3abfd89824
onth0s <metaagnosis@gmail.com>
2021-01-06 16:26:49 +0100

    test

---
+++ src/test.js
@@ -0,0 +1,22 @@
+let tags = [
+    {
+        tag: 'pensar',
+        description: 'Lo que haces cuando hablas sin mover la boca.'
+    },
+    {
+        tag: 'cantar',
+        description: 'Hacer sonidos con la boca que parecen música.'
+    },
+    {
+        tag: 'correr',
+        description: 'Lo que haces cuando jadeas.'
+    },
+    {
+        tag: 'saltar',
+        description: 'Normalmente no abres la boca cuando haces esto.'
+    }
+];
+
+let reg = [
+    
+];
