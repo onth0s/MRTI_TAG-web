52604979030bc0a064998a64b2d74abd7814702c
onth0s <metaagnosis@gmail.com>
2021-01-06 11:21:15 +0100

    solo falta el scroll y los tags

--- src/App.js
+++ src/App.js
@@ -8,6 +8,8 @@ function App() {
     
     const [isTextAreaDisabled, setIsTextAreaDisabled] = useState(false);
 
+    const [tag, setTag] = useState({});
+
     const handleSubmit = (e) => {
         e.preventDefault();
         
@@ -18,6 +20,8 @@ function App() {
         const temp = document.querySelector('.top .output');
         temp.scrollTop = temp.scrollHeight;
 
+        console.log(registry);
+
         setInput('');
     }
     
@@ -27,18 +31,22 @@ function App() {
         let arr2 = [];
         for (let i = 0; i < arr.length; i++) {
             if (arr[i] === "") {
-                arr2.push(<br/>);
+                arr2.push(<br key={i}/>);
             } else {
-                // arr2.push(<pre key={i}>[{counter}] {arr[i]}</pre>);
                 arr2.push(<pre key={i}>{arr[i]}</pre>);
             }         
         }
         counter++
 
-        return <div className="reg" onClick={(e) => {
-            console.log(e.target);
-            e.target.style.backgroundColor = 'red';
-        }} key={new Date().getTime().toString()}>{arr2}</div>;
+        return (
+            <div className="reg" onClick={(e) => {
+
+                console.log(e.currentTarget.innerHTML);
+            
+            }} key={new Date().getTime().toString()}>
+                {arr2}
+            </div>
+        );
     }
 
     return (
@@ -47,31 +55,41 @@ function App() {
                 <div className="overlay">
                     <p>A message that will be displayed when you fuck something up.</p>
                     <i className="fas fa-times" onClick={() => {
-                        document.querySelector('.overlay').style.visibility = 'hidden';
+                        const overlay = document.querySelector('.overlay');
+                        overlay.style.animation = 'fade-out 0.4s';
+                        overlay.addEventListener("animationend", () => {
+                            overlay.style.visibility = 'hidden';
+                            overlay.style.animation = 'none';
+                        });
                         setIsTextAreaDisabled(false);
                     }}></i>
                 </div>
 
-                <div className="output">
-                    {registry}
-                </div>
+                <div className="output">{registry}</div>
 
-                <div className="column"></div>
+                <div className="column">
+                    <div className="tags">
+                        <div>palabra</div>
+                        <div>idea</div>
+                        <div>razón</div>
+                        <div>pensar</div>
+                    </div>
+                </div>
             </div>
 
             <form onSubmit={handleSubmit}>
                 <textarea name="" id="" value={input} onChange={(e) => {
                     setInput(e.target.value);
                 }} onKeyPress={(e) => {
-                    if (e.keyCode === 13) {
-                        console.log('this is working');
+                    if (e.key === "Enter" && !e.shiftKey) {
+                        handleSubmit(e);
                     }
                 }} spellCheck="false" autoFocus disabled={isTextAreaDisabled}></textarea>
 
                 <button onClick={(e) => {
                     handleSubmit(e);
                     document.querySelector('.container form textarea').focus();
-                }}>Long Button Name</button>
+                }}>PUSH ↑</button>
             </form>
         </div>
     );
